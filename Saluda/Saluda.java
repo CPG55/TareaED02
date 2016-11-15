bb2353cdfbec9b78eb57806bbef774cc2d997622
CPG55 <carlos_pg_@hotmail.com>
2016-11-15 06:34:41 +0100

    Creamos la variable de clase "nombre"

--- Saluda/Saluda.java
+++ Saluda/Saluda.java
@@ -5,16 +5,14 @@ public class Saluda {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Variable de clase.
 		String nombre = null;
 
 		Scanner entrada = new Scanner(System.in);
 		
-		System.out.println("Cual es tu nombre?");
-		
-		nombre = entrada.nextLine();
-		
+		System.out.println("Cual es tu nombre?");		
+		nombre = entrada.nextLine();		
 		System.out.println("Hola " + nombre + " !!!");		
 		
 	}
-
 }
