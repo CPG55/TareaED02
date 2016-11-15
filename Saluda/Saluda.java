28fd046fc2265d167fd88718add0f4feb7161431
CPG55 <carlos_pg_@hotmail.com>
2016-11-15 06:12:22 +0100

    Saludo Personal

---
+++ Saluda/Saluda.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Saluda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String nombre = null;
+
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Cual es tu nombre?");
+		
+		nombre = entrada.nextLine();
+		
+		System.out.println("Hola " + nombre + " !!!");		
+		
+	}
+
+}
